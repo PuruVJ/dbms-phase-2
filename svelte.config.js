131bd5c370af065877919b2a7bbab4f446ac7ada
Puru Vijay <devpuruvj@gmail.com>
2022-05-03 01:51:17 +0530

    Remove edge

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({ edge: true, split: true })
+		adapter: adapter({ split: true })
 	},
 	preprocess: preprocess()
 };
