2bb87ff70964111e406d16764edcb1cb906580bc
Puru Vijay <devpuruvj@gmail.com>
2022-05-03 01:39:41 +0530

    Endpoint

---
+++ src/routes/book.js
@@ -0,0 +1,16 @@
+import { supabase } from '$lib/supabase';
+
+/** @type {import('./book').RequestHandler} */
+export const post = async ({ request }) => {
+	const form = await request.formData();
+	console.log(form.get('movie_date'));
+
+	await supabase.from('tickets').insert({
+		movie_time: new Date(`${form.get('movie_date')} ${form.get('movie_time')}`).toISOString(),
+		screen_name: form.get('screen_name'),
+		seat_no: form.get('seat_no'),
+		theatre: form.get('theatre')
+	});
+
+	return {};
+};
